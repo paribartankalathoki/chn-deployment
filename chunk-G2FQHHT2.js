24dd8cc31540345da085a1889228ff444799851d
Paribartan Kalathoki <>
2024-08-17 08:45:59 +0000

    Clean deploy Angular app to GitHub Pages

--- chunk-G2FQHHT2.js
+++
@@ -1,1 +0,0 @@
-import{f as a}from"./chunk-7HPZL72N.js";import"./chunk-C2RHBU3D.js";export{a as CollegeHubHomeModule};
---
+++ collegeHubNepal/browser/chunk-O6XHI3VC.js
@@ -0,0 +1,1 @@
+import{f as a}from"./chunk-YZZGS7UE.js";import"./chunk-ZQXDTH3J.js";export{a as CollegeHubHomeModule};
